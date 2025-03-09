edde42c729eb9c3497027bb2210e6c648d9e6b9f
AswathGopal <aswathgopal007@gmail.com>
2025-03-09 11:23:26 +0530

    changes upto booking of parking slot

--- apps/web/src/app/booking-failed/page.tsx
+++ apps/web/src/app/booking-failed/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <div>Booking failed.</div>
-  }
+  return <div>Booking failed.</div>
+}
--- libs/ui/src/components/molecules/Toast.tsx
+++ libs/ui/src/components/molecules/Toast.tsx
@@ -1,11 +1,11 @@
 import {
-    ToastContainer as ReactToastifyContainer,
-    toast,
-    Slide,
-  } from 'react-toastify'
-  
-  export const ToastContainer = () => (
-    <ReactToastifyContainer transition={Slide} />
-  )
-  
-  export { toast }
+  ToastContainer as ReactToastifyContainer,
+  toast,
+  Slide,
+} from 'react-toastify'
+
+export const ToastContainer = () => (
+  <ReactToastifyContainer transition={Slide} />
+)
+
+export { toast }
